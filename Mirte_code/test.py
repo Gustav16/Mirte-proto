543d3f5e5fdd9430794c73df35000759dbaae3e7
TobyDIKU <zrn331@alumni.ku.dk>
2026-09-05 13:39:16 +0200

    new

--- Mirte_code/test.py
+++ Mirte_code/test.py
@@ -41,7 +41,7 @@ while (True):
         mirte.drive(LT_lin_speed, LT_scaler*LT_ang_speed, LT_time)
 
 OM_lin_speed = 0.35
-OM_ang_speed = -0.0036
+OM_ang_speed = -0.00363
 OM_time = 2.7
 
 LT_lin_speed = 0

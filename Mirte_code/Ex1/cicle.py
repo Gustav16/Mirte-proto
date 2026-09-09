d1f2137a800b2c4d62bbb2b303478cdddbac2b19
Gustav <gustav@lundstroem.dk>
2026-09-09 14:27:07 +0200

    updated paths

--- Mirte_code/Ex1/cicle.py
+++ Mirte_code/Ex1/cicle.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../Mirte/ku_mirte_py
 
 import time
 from time import sleep
-from Mirte.ku_mirte_python.ku_mirte import KU_Mirte
+from ku_mirte import KU_Mirte
 
 #init mirte mirte
 mirte = KU_Mirte()
--- Mirte_code/Ex2/DriveAroundObstacle.py
+++ Mirte_code/Ex2/DriveAroundObstacle.py
@@ -4,7 +4,7 @@ import math
 import time
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../Mirte/ku_mirte_python'))
-from Mirte.ku_mirte_python.ku_mirte import KU_Mirte
+from ku_mirte import KU_Mirte
 
 
 mirte = KU_Mirte()

be733327f4d77fd1257c8568ec93c41b1edd66fb
mullk <mullk@live.dk>
2026-09-16 16:09:18 +0200

    Update map.py

--- Mirte_code/Ex3/map.py
+++ Mirte_code/Ex3/map.py
@@ -8,12 +8,15 @@ import math
 import time
 import numpy as np
 
+
 sys.path.append(
     os.path.join(
         os.path.dirname(__file__),
         '../../Mirte/ku_mirte_python'
     )
 )
+
+from ku_mirte import KU_Mirte
 
 time.sleep(1)  # wait for camera to setup
 

dba075f866f0b29b12b81c941cee4e2f6c8cb812
TobyDIKU <zrn331@alumni.ku.dk>
2026-09-23 14:20:56 +0200

    negative dx

--- Mirte_code/Ex4/rrt/mirte_rrt.py
+++ Mirte_code/Ex4/rrt/mirte_rrt.py
@@ -221,7 +221,7 @@ def Execute_path(path, mirte):
             continue
 
         # Direction towards next point
-        target_theta = np.arctan2(dx, dy)
+        target_theta = np.arctan2(-dx, dy)
 
         # Required rotation
         dtheta = target_theta - current_pose[2]
